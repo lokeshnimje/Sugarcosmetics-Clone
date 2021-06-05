0558e74c3f8c8cca269128f58868d9ce113e9e1f
Lokesh Nimje <lokesh.nimje@gmail.com>
2021-06-05 20:20:35 +0530

    changes added

--- Header&Footer/H&F.js
+++ Header&Footer/H&F.js
@@ -601,31 +601,25 @@ function displayClearance(data){
 //---------------------------------------clearance div starts here-------------------------------------------//
 
 //---------------------------------------slides for top carousel starts here-------------------------------------------//
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+var slideIndex = 0;
+showSlides();
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 //---------------------------------------slides for top carousel ends here-------------------------------------------//
 
